532c36a6521b38bde4864507d9f57d0ad47d48ee
mithun9421 <mithunmuralee94@gmail.com>
2016-08-24 10:42:29 +0530

    Create leap.cpp

---
+++ leap.cpp
@@ -0,0 +1,13 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  int year;
+  cin>>year;
+  if(year%=400 || year%=4)
+        cout<<"Leap Year";
+    else
+        cout<<"Non leap year;"
+}
